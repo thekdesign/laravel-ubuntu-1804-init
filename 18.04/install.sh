e22e5365012a5f888305bf0c2aaeae3affb8a198
KevinXu <kevin830802@gmail.com>
2019-04-26 09:08:29 +0800

    :wrench: 改變 hirak/prestissimo 的寫法

--- 18.04/install.sh
+++ 18.04/install.sh
@@ -68,7 +68,8 @@ function install_others {
 function install_composer {
     wget https://dl.laravel-china.org/composer.phar -O /usr/local/bin/composer
     chmod +x /usr/local/bin/composer
-    sudo -H -u ${WWW_USER} sh -c  'cd ~ && composer config -g repo.packagist composer https://packagist.laravel-china.org && composer global require hirak/prestissimo'
+    sudo -H -u ${WWW_USER} sh -c  'cd ~ && composer config -g repo.packagist composer https://packagist.laravel-china.org'
+    sudo -H -u ${WWW_USER} sh -c  'cd ~ && composer global require hirak/prestissimo'
 }
 
 call_function init_system "正在初始化系統" ${LOG_PATH}
