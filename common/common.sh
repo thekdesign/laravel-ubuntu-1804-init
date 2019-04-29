2be3a64820d2c761d97bf268fecaeaaf32168fbc
KevinXu <kevin830802@gmail.com>
2019-04-29 13:32:44 +0800

    :wrench: 隱藏 random_string

--- common/common.sh
+++ common/common.sh
@@ -23,7 +23,7 @@ function call_function {
     echo ']'
 }
 
-random_string(){
-    length=${1:-32}
-    echo `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${length} | head -n 1`
-}
+#random_string(){
+#    length=${1:-32}
+#    echo `cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${length} | head -n 1`
+#}
